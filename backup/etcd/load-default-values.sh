89893735691aa10e580b4c747e499f7113981c85
iofirag <iofirag@gmail.com>
2021-07-02 16:00:27 +0300

    fix

--- backup/etcd/load-default-values.sh
+++ backup/etcd/load-default-values.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Delete all etcd keys"
 ETCDCTL_API=3
-etcdctl del "*" --from-key=true  --endpoints=http://etcd:2380
+etcdctl del "*" --from-key=true --endpoints=http://etcd:2380
 #
 echo "putting default key-value-pairs into etcd"
 # Be careful: In properties file commented key-value pairs will be read out anyways (but with '#')
