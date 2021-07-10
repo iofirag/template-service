e06b1fb8763dafdaf1ca74b6448c2ce1d395f58f
iofirag <iofirag@gmail.com>
2021-07-10 21:59:16 +0300

    change port and based image

--- restart-services.sh
+++ restart-services.sh
@@ -1,9 +1,9 @@
 #REM Stop and remove last image build.
 #REM cd docker-services
-docker-compose down
+docker compose down
 #REM Build image again
 #REM This create our services using the configuration in docker-compose.yml and docker-compose.override.yml 
 #REM (but not the prod configuration in docker-compose.prod.yml).
-docker-compose build
+docker compose build
 #@REM docker-compose -f docker-compose.yml up --abort-on-container-exit
-docker-compose -f docker-compose.yml up
+docker compose -f docker-compose.yml up
