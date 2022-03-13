cc454eb4604a5b5f62f4dacfd6993ab3111aee99
iofirag <iofirag@gmail.com>
2022-03-13 02:51:26 +0200

    cc

--- services/rabbitmqService.js
+++ services/rabbitmqService.js
@@ -1,31 +1,126 @@
-const amqp = require('amqplib/callback_api');
+const _ = require('lodash');
+const amqp = require('amqplib');
+const opentracing = require('opentracing');
 
 module.exports = class RabbitMq {
-    constructor(logger, serviceData) {
+    constructor(config, serviceData, logger, tracer) {
+        this._config = config;
         this._serviceData = serviceData;
         this._logger = logger;
-        this._queueName = 'testQueue';
-        amqp.connect('amqps://ootbbpng:Q0oM7tMnJVGiGP_Vj6d4CvrsDoaiHnK5@cattle.rmq2.cloudamqp.com/ootbbpng', (error0, connection) => {
-            if (error0) {
-                throw error0;
-            }
-            this._conn = connection;
-            this._logger.log('info', 'connection open');
-
-            this._conn.createChannel((error1, channel) => {
-                if (error1) {
-                    throw error1;
-                }
-                channel.assertQueue(this._queueName, { durable: false });
-                this._logger.log('info', 'channel created');
-                this._channel = channel;
-            });
-        });
+        this._tracer = tracer;
     }
 
-    async sendMessage(msg) {
-        this._logger.log('info', `send message start' ${msg}`);
-        this._channel.sendToQueue(this._queueName, Buffer.from(JSON.stringify(msg)));
-        this._logger.log('info', `Sent: ${msg}`);
+    async start(parentSpan) {
+        let span;
+        try {
+            span = this._tracer.startSpan(`${this.constructor.name} - ${this.start.name}`, { childOf: parentSpan });
+            const connection = await amqp.connect(this._config.url);
+            await this.buildQueueMap(connection, this._config.queueMap);
+
+            // Catch connection errors
+            connection.on('error', (error) => {
+                this._logger.log('error', `${this.constructor.name} - ${this.start.name} failed. connection error: ${error.message}`);
+                throw error;
+            });
+        } catch (error) {
+            this._logger.log('error', `${this.constructor.name} - ${this.start.name} failed. error: ${error.message}`);
+            span.setTag(opentracing.Tags.ERROR, true);
+            throw error;
+        } finally {
+            span.finish();
+        }
+    }
+
+    async buildQueueMap(connection, queueMap) {
+        await this.buildFromConsumeList(connection, queueMap.consumeList);
+        await this.buildFromPublishMap(connection, queueMap.publishMap);
+    }
+
+    async buildFromConsumeList(connection, consumeList = []) {
+        for (const queueObj of consumeList) {
+            const queueName = queueObj.queueName;
+            const channel = await connection.createChannel();
+            channel.prefetch(parseInt(queueObj.prefetch));
+            // QueueMap
+            this._consumeMap[queueName] = channel;
+            if (queueObj.assertOptions) {
+                // Assert Queue
+                await this.assertionQueue(connection, queueObj);
+            }
+        }
+    }
+
+    async buildFromPublishMap(connection, publishMap = {}) {
+        if (!Object.keys(publishMap).length) return;
+        await this.createConfirmChannel(connection);
+        for (const queueKey in publishMap) {
+            if (publishMap[queueKey]) {
+                const queueObj = publishMap[queueKey];
+                if (queueObj.assertOptions) {
+                    // Assert Queue
+                    await this.assertionQueue(connection, queueObj);
+                }
+            }
+        }
+    }
+
+    async createConfirmChannel(connection) {
+        // Create confirm channel for all publishers
+        this._publishChannel = await connection.createConfirmChannel();
+    }
+
+    async assertionQueue(connection, queueObj) {
+        const assertChannel = await connection.createChannel();
+        const { durable, exclusive, deadLetterExchange, deadLetterRoutingKey, exchangeName, routingKey, exchangeType } = queueObj.assertOptions;
+        await assertChannel.assertExchange(exchangeName, exchangeType, {
+            ...(!_.isNil(durable) && { durable }),
+        });
+        await assertChannel.assetQueue(queueObj.queueName, {
+            ...(!_.isNil(exclusive) && { exclusive }),
+            ...(!_.isNil(deadLetterExchange) && { deadLetterExchange }),
+            ...(!_.isNil(deadLetterRoutingKey) && { deadLetterRoutingKey }),
+        });
+        await assertChannel.bindQueue(queueObj.queueName, exchangeName, routingKey);
+        await assertChannel.close();
+    }
+
+    async send(msg, queueName, parentSpan) {
+        let span;
+        try {
+            span = this._tracer.startSpan(`${this.constructor.name} - ${this.send.name}`, { childOf: parentSpan });
+            this._logger.log('info', `send messaage ${msg} to ${queueName}`);
+            await this._publishChannel.sendToQueue(queueName, Buffer.from(JSON.stringify(msg)), { persistent: true });
+        } catch (error) {
+            this._logger.log('error', `${this.constructor.name} - ${this.send.name} failed. error: ${error.message}`);
+            span.setTag(opentracing.Tags.ERROR, true);
+            throw error;
+        } finally {
+            span.finish();
+        }
+    }
+
+    async requeue(queueName, msg) {
+        await this.reject(queueName, msg, true);
+    }
+
+    async reject(queueName, msg, requeue = false) {
+        await this._consumeMap[queueName].reject(msg, requeue);
+    }
+
+    getPublishQueueNameByKey(key) {
+        return this._config.queueMap.publishMap[key].queueName;
+    }
+
+    async handleReceiveDone(deliveryTag, queueName, parentSpan) {
+        let span;
+        try {
+            span = this._tracer.startSpan(`${this.constructor.name} - ${this.handleReceiveDone.name}`, { childOf: parentSpan });
+            const ackObj = { fields: { deliveryTag } };
+            await this._consumeMap[queueName].ack(ackObj);
+        } catch (error) {
+            this._logger.log('error', `${this.constructor.name} - ${this.handleReceiveDone.name} failed. error: ${error.message}`);
+        } finally {
+            span.finish();
+        }
     }
 };
