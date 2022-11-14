ffbec588ae8378d9cc84c5ea984bce30c10457ed
iofirag <iofirag@gmail.com>
2022-11-14 22:36:00 +0200

    Update example1Controller.js

--- controllers/example1Controller.js
+++ controllers/example1Controller.js
@@ -2,9 +2,11 @@ const container = require('../containerConfig');
 
 const example1Service = container.resolve('example1Service');
 
-module.exports = {
-    addExample: example1Service.addExample.bind(example1Service),
-};
+module.exports.addExample = example1Service.addExample.bind(example1Service);
+
+// module.exports = {
+//     addExample: example1Service.addExample.bind(example1Service),
+// };
 
 // module.exports.addExample = (req, res) => {
 //     const params = res.locals.oas?.params;
