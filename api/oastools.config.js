070d5db5960450796f9ed1f0caa5d6ec86eac939
iofirag <iofirag@gmail.com>
2022-12-05 16:02:12 +0200

    ts compilation

--- api/oastools.config.js
+++ api/oastools.config.js
@@ -11,8 +11,6 @@
 //             url: "./api-docs/oas.yaml",
 //         },
 //         security: {
-//             auth: {
-                
-//             }
+//             auth: {}
 //     }
 // };
--- backup/etcd-initializer/Etcd-bitnami-initializer.Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM bitnami/etcd:latest
-COPY . /var/etcd
-WORKDIR /var/etcd
-CMD sh ./load-default-values.sh initial-values-local.properties
