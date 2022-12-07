c42c810c59c6bd73c030b8d279abc5306f242d9f
iofirag <iofirag@gmail.com>
2022-12-08 01:23:55 +0200

    removed files

--- src/controllers/example1Controller.js
+++
@@ -1,8 +0,0 @@
-const { container } = require('../containerConfig.ts');
-const { TYPES } = require('../containerTypes.ts');
-
-const example1Service = container.get(TYPES.Example1Service);
-
-module.exports = {
-    addExample: example1Service.addExample.bind(example1Service),
-};
